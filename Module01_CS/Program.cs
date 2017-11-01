b2a4a61b10552f8866d2d9f5ef526ff1a213bf46
ILaude <isabelle.laude@gmail.com>
2017-11-01 11:32:59 +0100

    Hej

--- Module01_CS/Program.cs
+++ Module01_CS/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             if (System.Diagnostics.Debugger.IsAttached)
+
+                System.Console.WriteLine("Hej!");
             {
                 System.Console.Write("Press any key to continue . . . ");
                 System.Console.ReadKey();
