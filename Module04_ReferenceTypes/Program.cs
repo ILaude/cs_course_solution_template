54ea74318eb2a916b4dce5781ca91ff66c8c3356
ILaude <isabelle.laude@gmail.com>
2017-11-02 11:16:43 +0100

    Start på ny opgave

--- Module04_ReferenceTypes/Program.cs
+++ Module04_ReferenceTypes/Program.cs
@@ -4,6 +4,23 @@
     {
         static void Main(string[] args)
         {
+            int[] månedsløn;
+            månedsløn = new int[5];
+            månedsløn[0] = 1000;
+            månedsløn[1] = 3000;
+            månedsløn[2] = 2000;
+            månedsløn[3] = 2000;
+            månedsløn[4] = 2000;
+
+            decimal gns = 0;
+            for (int i = 0; i < månedsløn.Length; i++)
+            {
+                gns += månedsløn[i];
+            }
+
+            gns = gns / månedsløn.Length;
+
+            System.Console.WriteLine(gns.ToString("n2"));
 
             if (System.Diagnostics.Debugger.IsAttached)
             {
