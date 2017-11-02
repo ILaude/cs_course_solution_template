e10ca355cc0ab69e65bfa5e9e074396f3a9db16c
ILaude <isabelle.laude@gmail.com>
2017-11-02 13:41:31 +0100

    Opgave med strenge

---
+++ OpgaveForskelVærdiReference/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpgaveForskelVærdiReference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person k1 = new Person();
+            k1.navn = "a";
+
+            Person k2 = new Person();
+            k2.navn = "b";
+
+            Console.WriteLine(k1.navn);
+            Console.WriteLine(k2.navn);
+
+            k1 = k2;
+            Console.WriteLine("k1=k2:");
+            Console.WriteLine(k1.navn);
+            Console.WriteLine(k2.navn);
+
+            k1.navn = "c";
+            Console.WriteLine("k1.navn=c:");
+            Console.WriteLine(k1.navn);
+            Console.WriteLine(k2.navn);
+
+        }
+    }
+    public class Person
+    {
+        public string navn;
+    }
+}
---
+++ OpgaveStrenge/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpgaveStrenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fornavn = "Mikkel";
+            string efternavn = "Cronberg";
+            string samletNavn = fornavn + " " + efternavn;
+            Console.WriteLine(samletNavn);
+
+            string navnStort = samletNavn.ToUpper();
+            Console.WriteLine(navnStort);
+
+            string navnLille = samletNavn.ToLower();
+            Console.WriteLine(navnLille);
+
+            string del = samletNavn.Substring(7, 4);
+            Console.WriteLine(del);
+            string[] deltNavn = samletNavn.Split(' ');
+
+            for (int i = 0; i < deltNavn.Length; i++)
+            {
+                Console.WriteLine(deltNavn[i]);
+            }
+
+            string adresse = "Rådhuspladsen\r\nKøbenhavn";
+            Console.WriteLine(adresse);
+
+            string filNavn = @"C:\temp\test.txt";
+            System.IO.File.WriteAllText(filNavn, samletNavn + "\r\n" + adresse);
+            
+
+        }
+    }
+}
