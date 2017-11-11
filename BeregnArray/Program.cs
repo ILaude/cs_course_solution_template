d69bbc9956421a9082d48b320dd29f17f31c4d05
Isabelle <Isabelle@T450S>
2017-11-11 18:10:16 +0100

    BeregnArray

---
+++ BeregnArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeregnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = { 10, 7, 6, 3, 1, 50, 3 };
+            var res = BeregnOgSortererArray(test);
+
+            Console.WriteLine(res.sum);
+            Console.WriteLine(res.gennemsnit.ToString("N2"));
+            
+        }
+        static ArrayResultat BeregnOgSortererArray(int[] array)
+        {
+            Array.Sort(array);
+            ArrayResultat resultat = new ArrayResultat();
+            resultat.sum = array.Sum();
+            resultat.gennemsnit = array.Average();
+            return resultat;
+        }
+    }
+    public struct ArrayResultat
+    {
+        public double sum;
+        public double gennemsnit;
+    }
+}
