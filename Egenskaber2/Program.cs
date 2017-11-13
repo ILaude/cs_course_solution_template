36759a57a47308513229fa3f8c34b67838dcc377
ILaude <isabelle.laude@gmail.com>
2017-11-13 15:26:23 +0100

    Egenskab2

---
+++ Egenskaber2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egenskaber2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trekant t = new Trekant(4, 2);
+            double areal = t.Areal;
+            Console.WriteLine(areal);
+
+            Trekant t2 = new Trekant(3, 5);
+            double areal2 = t2.ArealMetode();
+            Console.WriteLine(areal2);
+
+        }
+    }
+}
