6e7ff30750687413c8f0be0d8ea669e698a87c41
Isabelle <isabelle.laude@gmail.com>
2017-11-28 13:37:28 +0100

    Events

---
+++ Events/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kunde k = new Kunde() { KreditMax = -500, Saldo = 0 };
+            k.kreditoverskrevet += (s, e) => Console.WriteLine("Kredit overskrevet med " + e.overskrevetMed);
+            k.Køb(200);
+            k.Køb(500);
+
+        }
+    }
+
+    public class Kunde
+    {
+        public EventHandler<EventKredit> kreditoverskrevet;
+        //public EventHandler kreditoverskrevet = (s, e) => Console.WriteLine("Kredit overskrevet");
+        public string Navn { get; set; }
+        public int Saldo { get; set; }
+        public int KreditMax { get; set; }
+        public void Køb(int værdi)
+        {
+            Console.WriteLine("Kunde {0} køber for {1}", Navn, værdi);
+            this.Saldo -= værdi;
+            if (Saldo < KreditMax)
+            {
+                if (kreditoverskrevet != null)
+                {
+                    kreditoverskrevet(this, new EventKredit() {overskrevetMed = (KreditMax-Saldo) });
+                }
+            }
+        }
+    }
+
+    public class EventKredit : EventArgs
+    {
+        public int overskrevetMed { get; set; }
+    }
+}
---
+++ FilWatch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            System.IO.FileSystemWatcher w = new System.IO.FileSystemWatcher(@"c:\temp", "*.txt");
+            w.EnableRaisingEvents = true;
+            w.NotifyFilter = System.IO.NotifyFilters.FileName | System.IO.NotifyFilters.Size;
+            w.Created += (s, e) => Console.WriteLine("Filen {0} oprettes", e.Name);
+            w.Deleted += (s, e) => Console.WriteLine("Filen {0} slettes", e.Name);
+            w.Changed += (s,e) => Console.WriteLine("Filen {0} ændres", e.Name);
+
+
+            do
+            {
+
+            } while (true);
+        }
+    }
+}
